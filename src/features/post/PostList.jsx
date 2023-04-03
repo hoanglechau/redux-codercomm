070f455c2d4fbd7ce37cdec09e47539b9ca872d8
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-03 20:36:49 +0700

    Create PostList.jsx

---
+++ src/features/post/PostList.jsx
@@ -0,0 +1,44 @@
+import { LoadingButton } from "@mui/lab";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PostCard from "./PostCard";
+import { getPosts } from "./postSlice";
+
+function PostList({ userId }) {
+  const [page, setPage] = useState(1);
+  const { currentPagePosts, postsById, isLoading, totalPosts } = useSelector(
+    (state) => state.post
+  );
+  const posts = currentPagePosts.map((postId) => postsById[postId]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (userId) dispatch(getPosts({ userId, page }));
+  }, [dispatch, userId, page, totalPosts]);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <PostCard key={post._id} post={post} userId={userId} />
+      ))}
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        {totalPosts ? (
+          <LoadingButton
+            variant="outlined"
+            size="small"
+            loading={isLoading}
+            onClick={() => setPage((page) => page + 1)}
+            disabled={Boolean(totalPosts) && posts.length >= totalPosts}
+          >
+            Load more
+          </LoadingButton>
+        ) : (
+          <Typography variant="h6">No post yet</Typography>
+        )}
+      </Box>
+    </>
+  );
+}
+
+export default PostList;
