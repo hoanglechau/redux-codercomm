f1c35ac2688048e42784e21fea910bfb637831a9
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-06 04:17:49 +0700

    Update Tabs.js

--- src/theme/customizations/Tabs.js
+++ src/theme/customizations/Tabs.js
@@ -9,48 +9,48 @@ function Tabs(theme) {
           borderTopRightRadius: theme.shape.borderRadius,
           textTransform: "capitalize",
           "&.Mui-selected": {
-            color: theme.palette.text.primary,
+            color: theme.palette.text.primary
           },
           "&:not(:last-of-type)": {
-            marginRight: theme.spacing(5),
+            marginRight: theme.spacing(5)
           },
           "@media (min-width: 600px)": {
-            minWidth: 48,
-          },
+            minWidth: 48
+          }
         },
         labelIcon: {
           minHeight: 48,
           flexDirection: "row",
           "& > *:first-of-type": {
             marginBottom: 0,
-            marginRight: theme.spacing(1),
-          },
+            marginRight: theme.spacing(1)
+          }
         },
         wrapper: {
           flexDirection: "row",
-          whiteSpace: "nowrap",
+          whiteSpace: "nowrap"
         },
         textColorInherit: {
           opacity: 1,
-          color: theme.palette.text.secondary,
-        },
-      },
+          color: theme.palette.text.secondary
+        }
+      }
     },
     MuiTabPanel: {
       styleOverrides: {
         root: {
-          padding: 0,
-        },
-      },
+          padding: 0
+        }
+      }
     },
     MuiTabScrollButton: {
       styleOverrides: {
         root: {
           width: 48,
-          borderRadius: "50%",
-        },
-      },
-    },
+          borderRadius: "50%"
+        }
+      }
+    }
   };
 }
 
