ff2d4f8413d51b46f273776dba87d3f52bd36da8
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-06 04:15:40 +0700

    Update FTextField.jsx

--- src/components/form/FTextField.jsx
+++ src/components/form/FTextField.jsx
@@ -1,5 +1,5 @@
-import { useFormContext, Controller } from "react-hook-form";
 import { TextField } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
 
 function FTextField({ name, ...other }) {
   const { control } = useFormContext();
